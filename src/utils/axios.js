26741c701a4d1cd8aabbed4067d78243a4d48218
ofega <fegaeze@gmail.com>
2020-04-16 08:52:33 +0100

    setup axios instance

---
+++ src/utils/axios.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export const baseURL = process.env.REACT_APP_API_HOST;
+
+
+export const axiosWithAuth = () => {
+  const token = localStorage.getItem("token");
+
+  const instance = axios.create({
+    baseURL,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `${token}`,
+    },
+  });
+
+  return instance;
+};
