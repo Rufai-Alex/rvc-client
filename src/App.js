8d9d48ceebeb5781974cbbddf526daf5bea77320
ofega <fegaeze@gmail.com>
2020-04-17 06:35:37 +0100

    material-ui setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
 
+import store from './state';
+import theme from './theme';
 import AppRouter from './router';
 
 const App = () => {
-  return <AppRouter />;
+  return (
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppRouter />
+      </ThemeProvider>
+    </Provider>
+  );
 };
 
 export default App;
